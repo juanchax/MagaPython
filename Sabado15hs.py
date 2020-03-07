a335b0f52146ba779a7f2c3820dd18cb6cd91fd4
juanchax <juanchax@gmail.com>
2020-03-07 02:42:32 -0300

    Week 1

--- Sabado15hs.py
+++ Sabado15hs.py
@@ -1,42 +1,6 @@
 '''
 
 
-# 06 -----
-# Crear un programa que calcule cuánto dinero tendré
-# al cabo de un mes si deposito hoy 2000 en el banco
-# y el interés mensual es de 5%, y luego devuelva por pantalla ese valor.
-
-initial_deposit = 2000
-monthly_interest = 1.05
-
-total_upToDate = initial_deposit * (monthly_interest ** 1)
-
-print(total_upToDate)
-
-
-# 07 -----
-# Crear un programa que calcule el sueldo bruto de una persona que trabaja de lunes a viernes 8 hs
-# y su pago por hora es de 400 pesos.
-# Devolver el sueldo por pantalla.
-
-hourly_rate = 400
-weekly_hours = 8 * 5
-weeks_month = 4
-salary = weekly_hours * weeks_month
-
-print(salary)
-
-
-
-# 08 -----
-# Crear un programa que almacene en una lista las siguientes materias:
-# Historia, Matemática, Lengua y Geografía.
-# Luego devolver por pantalla la última materia almacenada.
-
-list_a = ["Historia", "Matemática", "Lengua", "Geografía"]
-print(list_a[-1])
-
-
 # 09 -----
 # Crear un programa que pregunte al usuario 5 números enteros
 # y devuelva una lista con ellos.
@@ -47,43 +11,6 @@ print("A continuación deberá ingresar 5 números enteros. /nPor favor presione
 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-
-# 10 -----
-# Escribir un programa que almacene todas las letras del abecedario
-# y luego elimine las vocales
-# y nos devuelva una lista sin las vocales, sin modificar la original.
-
-import string
-
-
-alphabet = list(string.ascii_lowercase)
-
-
-def del_vowels (list):
-    lphbt = []
-    vowels = ("a", "e", "i", "o", "u")
-    for x in list:
-        if x not in vowels :
-            lphbt.append(x)
-    print(lphbt)
-
-
-del_vowels(alphabet)
-
-
-
-# 11 -----
-# Crear una lista con varios números,
-# luego ordenarlos de manera creciente
-# y devolver por pantalla la lista ordenada
-# y cuál es el menor y cuál el mayor.
-
-num_list = [1, 3, 5, 56, 34, 78, 2, 87, 33, 4576, 231, 4, 50, 1239, 345, 76986, 90]
-print(sorted(num_list))
-print(min(num_list))
-print(max(num_list))
-
 
 
 
@@ -97,23 +24,6 @@ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-
-
-
-# 13 -----
-# Crear un programa que guarde en una variable
-# el diccionario {'Euro':'€', 'Dollar':'$', 'Yen':'¥'},
-# pregunte al usuario por una divisa
-# y muestre su símbolo o un mensaje de aviso
-# si la divisa no está en el diccionario.
-
-
-currency_sign = {"Euro": "€", "Dollar": "$", "Yen": "¥", "Pound": "£"}
-usr_input = input("Por favor ingrese el nombre de la divisa que desea: ")
-if usr_input.capitalize() not in currency_sign:
-    print("La divisa solicitada no se encuentra en archivo. ")
-else:
-    print(currency_sign[usr_input.capitalize()])
 
 '''
 
