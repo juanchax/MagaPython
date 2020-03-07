6951a515e6ccd19228660c4a26bf97bd879b6a43
juanchax <juanchax@gmail.com>
2020-03-07 02:32:14 -0300

    Week 1

---
+++ Week 1/07. Calculate Salary.py
@@ -0,0 +1,14 @@
+# 07 -----
+# Create a rpgram that calculates the gross salary of a person that
+# works mon-fri, 8hr a day; and is paid $400 and hour. Print out the
+# Monthly total.
+#
+# Crear un programa que calcule el sueldo bruto de una persona que trabaja de lunes a viernes 8 hs
+# y su pago por hora es de 400 pesos. Devolver el sueldo por pantalla.
+
+hourly_rate = 400
+weekly_hours = 8 * 5
+weeks_month = 4
+salary = weekly_hours * weeks_month * hourly_rate
+
+print(salary)
