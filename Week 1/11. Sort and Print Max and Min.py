96f9bf32c729f066eee7f557ef95af4d7897a844
juanchax <juanchax@gmail.com>
2020-03-07 02:27:09 -0300

    Week 1

---
+++ Week 1/11. Sort and Print Max and Min.py
@@ -0,0 +1,12 @@
+# 11 -----
+# Build a list with several numbers, sort them ascending
+# andprint out: the sorted list, the smallest number and the
+# largest number (one per line)
+#
+# Crear una lista con varios números, luego ordenarlos de manera creciente
+# y devolver por pantalla la lista ordenada y cuál es el menor y cuál el mayor.
+
+num_list = [1, 3, 5, 56, 34, 78, 2, 87, 33, 4576, 231, 4, 50, 1239, 345, 76986, 90]
+print(sorted(num_list))
+print(min(num_list))
+print(max(num_list)) 
