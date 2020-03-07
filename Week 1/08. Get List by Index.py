0aaa61dcc6ab83ebf49d94d2787abd6a505ba752
juanchax <juanchax@gmail.com>
2020-03-07 02:34:36 -0300

    Week 1

---
+++ Week 1/08. Get List by Index.py
@@ -0,0 +1,11 @@
+# 08 -----
+# Create a program that stores the following subjects in a list:
+# History, Maths, English and Geography.
+# Print out the last subject stored
+#
+# Crear un programa que almacene en una lista las siguientes materias:
+# Historia, Matemática, Lengua y Geografía.
+# Luego devolver por pantalla la última materia almacenada.
+
+list_a = ["History", "Maths", "English", "Geography"]
+print(list_a[-1])
