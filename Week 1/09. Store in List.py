a1f914a228acf39a5a75c3b8552ed1ad1073a8be
juanchax <juanchax@gmail.com>
2020-03-11 15:03:57 -0300

    Week 1

--- Week 1/09. Store in List.py
+++ Week 1/09. Store in List.py
@@ -2,4 +2,31 @@
 # Crear un programa que pregunte al usuario 5 números enteros
 # y devuelva una lista con ellos.
 
-print("A continuación deberá ingresar 5 números enteros. /nPor favor presione la tecla 'Enter' luego de cada número.")
+# Verify that the input is an integer, if not, try again
+def inputNumber(num):
+    while True:
+        try:
+            usrInput = int(input(num))
+        except ValueError:
+            print("El dato ingresado no es un número entero. Por favor intente nuevamente. ")
+            continue
+        else:
+            return usrInput
+            break
+
+
+# build the integer list, print out the contents when done
+def buildList(rangex, list):
+    for i in range(int(rangex)):
+        intNum = inputNumber("Por favor ingrese un número entero: ")
+        list.append(intNum)
+    else:
+        print(list)
+
+# main program
+intList = []
+usrRange = 5
+
+print("A continuación deberá ingresar" + str(usrRange) + "números enteros. "
+      "/nPor favor presione la tecla 'Enter' luego de cada número.")
+buildList(usrRange, intList)
