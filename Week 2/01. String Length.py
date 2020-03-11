ab8bc2f47f6948607ee59483338a3d882b33f87c
juanchax <juanchax@gmail.com>
2020-03-11 20:09:05 -0300

    Week 1

---
+++ Week 2/01. String Length.py
@@ -0,0 +1,4 @@
+# 01 ------
+# Pedir al usuario que ingrese un mensaje cualquiera, si el mensaje tiene más de 100 caracteres
+# imprimirlo por pantalla, si tiene entre 50 y 100 caracteres imprimirlo al revés, si no se cumple
+# ninguna de las opciones anteriores, por pantalla devolver un mensaje que diga "su mensaje es demasiado corto".
