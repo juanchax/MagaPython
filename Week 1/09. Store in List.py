069d80a47120a39d4116b26b1cd0fbaff51fd166
juanchax <juanchax@gmail.com>
2020-03-11 14:23:46 -0300

    Week 1

---
+++ Week 1/09. Store in List.py
@@ -0,0 +1,5 @@
+# 09 -----
+# Crear un programa que pregunte al usuario 5 números enteros
+# y devuelva una lista con ellos.
+
+print("A continuación deberá ingresar 5 números enteros. /nPor favor presione la tecla 'Enter' luego de cada número.")
