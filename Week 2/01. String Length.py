b0f3ab00176edae9f49f2f43f1bd528e8a15f59a
juanchax <juanchax@gmail.com>
2020-03-23 21:48:05 -0300

    Default Changelist

--- Week 2/01. String Length.py
+++ Week 2/01. String Length.py
@@ -2,3 +2,25 @@
 # Pedir al usuario que ingrese un mensaje cualquiera, si el mensaje tiene más de 100 caracteres
 # imprimirlo por pantalla, si tiene entre 50 y 100 caracteres imprimirlo al revés, si no se cumple
 # ninguna de las opciones anteriores, por pantalla devolver un mensaje que diga "su mensaje es demasiado corto".
+
+def inputMessage():
+
+    message = input("Please enter you message: ")
+    messageLength = len(message)
+
+
+    if messageLength > 100 :
+
+        print(message)
+
+    elif messageLength > 50 :
+
+        messageReversed = ''.join(reversed(message))
+        print(messageReversed)
+
+    else:
+
+        print("Your message is too short. ")
+
+
+inputMessage()
