a5f8063ad0b26db5d02f598c61f065b18af504c9
juanchax <juanchax@gmail.com>
2020-03-25 21:12:51 -0300

    Default Changelist

--- Week 3/05. Check Number.py
+++ Week 3/05. Check Number.py
@@ -7,10 +7,26 @@
 
 def inputParams():
 
+    # function to get input and build a list of input parameters
+    # with leading and trailing spaces stripped
+
     inParams = input("Please enter your commma-separated parameters: ")
+
+    # build param list
+    inParamsList = inParams.split(',')
+
+    # build list with trimmed trailing and leading spaces
+    paramsList = []
+    for i in inParamsList:
+        paramsList.append(i.strip())
+
+    return paramsList
 
 
 def isNumber(param):
+
+    # function to check if a certain parameter
+    # is a number (integer or float)
 
     try:
         isInt = int(param)
@@ -21,5 +37,32 @@ def isNumber(param):
             return 1
         except ValueError:
             return  0
+
+
+def buildTrueFalseList(paramList):
+
+    # build list with '1' for TRUE and '0' for FALSE
+
+    listTorF = []
+    for i in paramList:
+         listTorF.append(isNumber(i))
+
+    return listTorF
+
+
+def printResult(boolList):
+
+    # function to check if the params are all, some or
+    # none numeric values
+
+    if len(boolList) == sum(boolList):
+        print("True")
+
+    elif sum(boolList) == 0:
+        print("False")
+
+    else:
+        print("None")
+
 
 
