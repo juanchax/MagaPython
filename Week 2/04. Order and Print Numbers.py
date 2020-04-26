a6052586ccb2ab3d9e8570edfd869d09a210c9da
juanchax <juanchax@gmail.com>
2020-04-26 13:26:55 -0300

    Default Changelist

--- Week 2/04. Order and Print Numbers.py
+++ Week 2/04. Order and Print Numbers.py
@@ -8,13 +8,14 @@ numList = []
 for i in range (6):
 
     number = input("Por favor ingrese un numero entero: ")
-    numList.append(number)
+    numList.append(int(number))
 
 # Si uso numList.sort() no me hace el sorting
 # si trato de crear una nueva lista sortedList = numList.sort() me tira un exception:
 # TypeError: 'NoneType' object is not iterable
 
-sorted(numList)
+#sorted(numList)
+numList.sort()
 
 for number in numList:
     print(number)
