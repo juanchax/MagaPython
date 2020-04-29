8d5220eda3d06a865daae7dca7767a3936452661
juanchax <juanchax@gmail.com>
2020-04-29 18:12:01 -0300

    Update 09. Calculate CC Limit.py

--- Week 2/09. Calculate CC Limit.py
+++ Week 2/09. Calculate CC Limit.py
@@ -5,3 +5,11 @@
 # le queda luego de efectuarla. Si no puede, indicar cuánto dinero le falta para
 # poder realizarla.
 
+monto_disponible = float(input('Por favor ingrese el monto disponible de su tarjeta de crédito (numeros solamente): '))
+
+monto_compra = 2500
+
+if monto_disponible >= monto_compra:
+    print('Compra existosa, su saldo restante es: $%s' % abs(monto_disponible - monto_compra))
+else:
+    print('Usted no dispone de suficiente dinero para realizar la compra. Su límite de compra con el disponible actual es: $%s' % abs(monto_disponible - monto_compra))
