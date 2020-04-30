8a43dcab5b1c46092d394dfd6b4b0f207f01bc71
juanchax <juanchax@gmail.com>
2020-04-29 21:13:37 -0300

    Update 02. Funtion Even Numbers.py

--- Week 3/02. Funtion Even Numbers.py
+++ Week 3/02. Funtion Even Numbers.py
@@ -2,3 +2,11 @@
 #
 # Crear una función que devuelva una lista con todos los números
 # pares del 0 al 100 inclusive. Imprimir esa lista por pantalla.
+
+def print_evens(upper_limit):
+    upper_limit = int(upper_limit)
+    for number in range(0, upper_limit +1):
+        if number % 2 == 0:
+            print(number)
+
+print_evens(100)
